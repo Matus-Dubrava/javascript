33847dbd749b851490dccc27aa74c18a09123c08
matus dubrava <matusdubrava@gmail.com>
2018-10-31 17:48:42 +0100

    updated KNN for multidimensional data points

--- ml/tutorials/MLKits/plinko/score.js
+++ ml/tutorials/MLKits/plinko/score.js
@@ -1,8 +1,63 @@
+const outputs = [];
+
+// 1-D distance function
+// function distance(pointA, pointB) {
+//     return Math.abs(pointA - pointB);
+// }
+
+// multidimensional distance function
+function distance(pointA, pointB) {
+    // compute distance using pythagorean algorithm
+
+    return (
+        _.chain(pointA)
+            .zip(pointB)
+            .map(([a, b]) => (a - b) ** 2)
+            .sum()
+            .value() ** 0.5
+    );
+}
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+    outputs.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function splitDataset(data, testCount) {
+    const shuffled = _.shuffle(data);
+
+    const testSet = _.slice(shuffled, 0, testCount);
+    const trainingSet = _.slice(shuffled, testCount);
+
+    return [testSet, trainingSet];
+}
+
+function knn(data, point, k) {
+    return _.chain(data)
+        .map(row => [distance(row[0], point), row[3]])
+        .sortBy(row => row[0])
+        .slice(0, k)
+        .countBy(row => row[1])
+        .toPairs()
+        .sortBy(row => row[1])
+        .last()
+        .first()
+        .parseInt()
+        .value();
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
-}
+    const testSetSize = 100;
+    const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
+    _.range(1, 20).forEach(k => {
+        const accuracy = _.chain(testSet)
+            .filter(
+                testPoint => testPoint[3] === knn(trainingSet, testPoint[0], k)
+            )
+            .size()
+            .divide(testSetSize)
+            .value();
+
+        console.log('For k of:', k, 'Accuracy:', accuracy);
+    });
+}
